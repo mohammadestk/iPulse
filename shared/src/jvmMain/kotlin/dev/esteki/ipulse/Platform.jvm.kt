273d736ba73bf21f888355c20ff9959170f870a5
mohammadesteki <mr.estek@yahoo.com>
2026-07-05 15:58:42 +0330

    remove unused files

--- shared/src/jvmMain/kotlin/dev/esteki/ipulse/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package dev.esteki.ipulse
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
