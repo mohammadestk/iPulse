f2403993efb829b91809a05bcdab11fd20998ae0
mohammadesteki <mr.estek@yahoo.com>
2026-07-05 16:00:14 +0330

    Revert "Revert "remove unused files""
    This reverts commit 0a1061ad85b2fd6191dea66f77e959ad7a93429b.

--- shared/src/commonMain/kotlin/dev/esteki/ipulse/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.esteki.ipulse
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return sayHello(platform.name)
-    }
-}
--- shared/src/commonMain/kotlin/dev/esteki/ipulse/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package dev.esteki.ipulse
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
